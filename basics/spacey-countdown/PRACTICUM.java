aa4d2723c4284cbe2ed2fdb10c2ffd8175aeb69c
Dmitry Vitsko <dmitryvitsko@yandex.ru>
2026-05-31 13:00:21 +0300

    Add SpaceY countdown task - for loop with custom step

---
+++ basics/spacey-countdown/PRACTICUM.java
@@ -0,0 +1,10 @@
+public class Practicum {
+    public static void main(String[] args) {
+        int secondsBeforeStart = 70;
+
+        for (int i = secondsBeforeStart; i >= 0; i -= 7) {
+            System.out.println("До старта SpaceY осталось " + i);
+        }
+        System.out.println("Поехали! Узнаем, есть ли жизнь на Марсе!");
+    }
+}
